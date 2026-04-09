b3e23bc5803835832b3413e46be03ece51a5e918
Edson Junior <soneddjr@gmail.com>
2026-04-09 16:05:26 +0200

    fix: password minLength is now 8

--- app/(auth)/signup/actions.ts
+++ app/(auth)/signup/actions.ts
@@ -29,7 +29,7 @@ const signUpSchema = z.object({
     ),
   password: z
     .string()
-    .min(6, 'Password must be at least 6 characters.')
+    .min(8, 'Password must be at least 8 characters.')
     .max(120),
 });
 
